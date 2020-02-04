802e9f03f4ef388594c9433db1e9bc279b2cf7eb
Anja Schrieber <anja.schrieber@gmail.com>
2020-02-04 20:13:42 +0100

    Solve task 3

--- main.js
+++ main.js
@@ -26,3 +26,34 @@ tabs.forEach(tab => {
     })
   })
 })
+
+getPrice()
+
+function getPrice() {
+  const input = document.querySelector('.quantity')
+  const button = document.querySelector('.calculate-btn')
+  const amount = document.querySelector('.amount')
+  const price = 19.95
+  const currentPrice = price - price * 0.1
+  const output = currentPrice.toFixed(2)
+
+  input.value = '1'
+  amount.innerHTML = `${output}  €`
+
+  button.addEventListener('click', () => {
+    const quantity = input.value
+    const sum = currentPrice * quantity
+    const discountFor = quantity => {
+      if (quantity >= 5) {
+        return 0.7
+      }
+      if (quantity >= 3) {
+        return 0.8
+      }
+      return 0.9
+    }
+    const total = sum * discountFor(quantity)
+    const output = total.toFixed(2)
+    amount.innerHTML = `${output}  €`
+  })
+}
